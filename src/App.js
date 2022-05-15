1d9380bf18002049cb76778968a0e65bd9a68161
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-15 16:54:02 -0500

    complete styles and components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Button from './components/Button';
+import ButtonClear from './components/ButtonClear';
+import Screen from './components/Screen';
 import dogebeta from './images/dogebeta.png'
 
 function App() {
@@ -13,14 +15,39 @@ function App() {
         />
       </div>
       <div className="container-calculator">
+        <Screen/>
         <div className="row">
           <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
           <Button>+</Button>
         </div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+        <div className="row">
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>*</Button>
+        </div>
+        <div className="row">
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
+        <div className="row">
+          <Button>√</Button>
+          <Button>%</Button>
+          <Button>x²</Button>
+          <ButtonClear>
+            Clear
+          </ButtonClear>
+        </div>
       </div>
     </div>
   );
