8ba5eef12b5e3bd12adb5bfc39aa1756f358fd75
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-15 13:41:01 -0500

    uploading project integrating features as css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import dogebeta from './images/dogebeta.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='baby-doge-contenedor'>
+        <img 
+        src={dogebeta} 
+        className='baby-doge-logo'
+        alt='Logo babydoge'
+        />
+      </div>
     </div>
   );
 }
