b6c6f735c957b95940800b26d53c3ab9b61409d4
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-15 14:34:50 -0500

    adding feature logical Button and styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+import Button from './components/Button';
 import dogebeta from './images/dogebeta.png'
 
 function App() {
   return (
     <div className="App">
-      <div className='baby-doge-contenedor'>
+      <div className='baby-doge-container'>
         <img 
         src={dogebeta} 
         className='baby-doge-logo'
         alt='Logo babydoge'
         />
+      </div>
+      <div className="container-calculator">
+        <div className="row">
+          <Button>1</Button>
+          <Button>+</Button>
+        </div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
       </div>
     </div>
   );
